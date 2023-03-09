e8081f9fe727c4b8a08355c2f5025b76d7349e48
Allister Beharry <allister.beharry@gmail.com>
2023-03-09 17:34:32 -0400

    Add Gui project.

---
+++ src/Stratis.DevEx.Gui/Program.cs
@@ -0,0 +1,22 @@
+﻿using Eto.Drawing;
+using Eto.Forms;
+using System;
+using System.Threading.Tasks;
+using H.Pipes;
+namespace Stratis.DevEx.Gui
+{
+    class Program
+    {
+        [STAThread]
+        static void Main(string[] args)
+        {
+            
+            new Application(Eto.Platform.Detect).Run(new MainForm());
+        }
+
+        static async Task Foo()
+        {
+            await using var server = new PipeServer<string>("ll");
+        }
+    }
+}
