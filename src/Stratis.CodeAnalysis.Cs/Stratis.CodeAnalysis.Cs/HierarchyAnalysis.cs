bb0db4c0e15b77913ebf98c5edcf57b04978c346
Allister Beharry <allister.beharry@gmail.com>
2023-06-23 12:24:35 -0400

    Add HierarchyAnalysis, CirrusTestt.

---
+++ src/Stratis.CodeAnalysis.Cs/Stratis.CodeAnalysis.Cs/HierarchyAnalysis.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.Operations;
+using Microsoft.CodeAnalysis.FlowAnalysis;
+
+using SharpConfig;
+
+using Stratis.DevEx;
+namespace Stratis.CodeAnalysis.Cs
+{
+    public class HierarchyAnalysis : Runtime
+    {
+        public static void AnalyzeControlFlow(string cfgFile, Configuration config, SemanticModel model)
+        {
+            using var top = Begin("Analyzing class hierarchy of source document {doc} using configuration {cfg}.", model.SyntaxTree.FilePath, config["General"]["ConfigFile"].StringValue);
+            var projectDir = Path.GetDirectoryName(config["General"]["ConfigFile"].StringValue);
+            //var graph = new Graph();
+            ClassDeclarationSyntax[] classdecls = model.SyntaxTree.GetRoot().DescendantNodes().OfType<ClassDeclarationSyntax>()
+                                   .ToArray();
+            foreach(var c in classdecls)
+            {
+                var t = model.GetDeclaredSymbol(c);
+                //t.
+            }
+        }
+    }
+}
---
+++ tests/Stratis.CodeAnalysis.Cs.Test/CirrusTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Immutable;
+using System.Threading.Tasks;
+using System.IO;
+
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.CSharp.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VerifyCS = Stratis.CodeAnalysis.Cs.Test.CSharpCodeFixVerifier<
+    Stratis.CodeAnalysis.Cs.SmartContractAnalyzer,
+    Stratis.CodeAnalysis.Cs.StratisCodeAnalysisCsCodeFixProvider>;
+
+namespace Stratis.CodeAnalysis.Cs.Test
+{
+    [TestClass]
+    public class CirrusTests
+    {
+        [TestMethod]
+        public async Task CanValidateAddressContract()
+        {
+            var code = File.ReadAllText(Path.Combine("..", "..", "..", "..", "..", "ext", 
+                "CirrusSmartContracts", "Mainnet", "AddressMapper", "AddressMapper", "AddressMapper.cs"));
+            await VerifyCS.VerifyAnalyzerAsync(code, System.Array.Empty<DiagnosticResult>());
+        }
+    }
+}
