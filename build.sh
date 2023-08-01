1bef2ef23d46701732fbda46f6004807430748f1
Allister Beharry <allister.beharry@gmail.com>
2023-08-01 12:30:13 -0400

    Fix path in build script.

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 set -e 
 
 dotnet restore src/Stratis.DevEx.sln && \
-dotnet build src/Stratis.CodeAnalysis.Cs/Stratis.CodeAnalysis.Cs/Stratis.CodeAnalysis.Package.csproj /p:Configuration=Release && \
+dotnet build src/Stratis.CodeAnalysis.Cs/Stratis.CodeAnalysis.Cs.Package/Stratis.CodeAnalysis.Cs.Package.csproj /p:Configuration=Release && \
 dotnet build src/Stratis.DevEx.Gui/Stratis.DevEx.Gui.csproj /p:Configuration=Release $*
 
