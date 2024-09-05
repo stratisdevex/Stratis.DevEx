ffc7769a630c96f5f72cceb0002d2c6c5c07c3a3
Allister Beharry <allister.beharry@gmail.com>
2024-09-05 09:08:44 -0400

    Fix bug with compier bin files. Correctly include node_modules dependencies dir when compiling contracts.

--- src/Stratis.VS.StratisEVM/SolidityCompiler.cs
+++ src/Stratis.VS.StratisEVM/SolidityCompiler.cs
@@ -13,17 +13,24 @@ namespace Stratis.VS.StratisEVM
 {
     public class SolidityCompiler : Runtime
     {
-        public static async Task CompileFileAsync(string file)
+        public static async Task CompileFileAsync(string file, string workspaceDir)
         {
             await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
             VSUtil.ShowLogOutputWindowPane(ServiceProvider.GlobalProvider, "Solidity Compiler");
+            VSUtil.LogInfo("Solidity Compiler", string.Format("Compiling {0} in {1}...", file, workspaceDir));
             var binfiles = Directory.GetFiles(AssemblyLocation, "*.bin", SearchOption.TopDirectoryOnly);
             foreach ( var binfile in binfiles ) 
             {
                 File.Delete(binfile);   
             }
+            
+           
             var cmd = "cmd.exe";
-            var args = "/c node " + Path.Combine("node_modules", "solc", "solc.js") + " \"" + file + "\" --bin";
+            var args = "/c node " + Path.Combine("node_modules", "solc", "solc.js") + " --base-path=\"" + workspaceDir + "\"" + " \"" + file + "\" --bin";
+            if (Directory.Exists(Path.Combine(workspaceDir, "node_modules")))
+            {
+                args += " --include-path=" + Path.Combine(workspaceDir, "node_modules");
+            }
             var output = await ThreadHelper.JoinableTaskFactory.RunAsync(async () => await RunCmdAsync(cmd, args, AssemblyLocation));
             if (CheckRunCmdError(output)) 
             {
@@ -45,30 +52,33 @@ namespace Stratis.VS.StratisEVM
             }
             else
             {
-                var s = Directory.GetFiles(AssemblyLocation, "*.bin", SearchOption.TopDirectoryOnly);
-                if (s is null || s.Length == 0) 
+                binfiles = Directory.GetFiles(AssemblyLocation, "*.bin", SearchOption.TopDirectoryOnly);
+                if (binfiles is null || binfiles.Length == 0) 
                 {
-                    VSUtil.LogError("Solidity Compiler", "Could not read Solidity compiler output file.");
-                    return;
-                }
-                else if (s.Length != 1)
-                {
-                    VSUtil.LogError("Solidity Compiler", "Error reading Solidity compiler output file: more than one output file found.");
-                    binfiles = Directory.GetFiles(AssemblyLocation, "*.bin", SearchOption.TopDirectoryOnly);
-                    foreach (var binfile in binfiles)
-                    {
-                        File.Delete(binfile);
-                    }
+                    VSUtil.LogError("Solidity Compiler", "Could not read Solidity compiler output. No compiler output files found.");
                     return;
                 }
                 else
                 {
-                    var binfile = s.Single();   
-                    var b = File.ReadAllText(binfile);
-                    File.Delete(binfile);
-                    VSUtil.LogInfo("Solidity Compiler", "======= " + file + "======= " +"\nBinary: \n" + b);
+                    string b = null;
+                    foreach (var binfile in binfiles)
+                    {
+                        if (binfile.Contains(Path.GetFileNameWithoutExtension(file)))
+                        {
+                            b = File.ReadAllText(binfile);   
+                            VSUtil.LogInfo("Solidity Compiler", "======= " + file + "======= " + "\nBinary: \n" + b);
+                        }
+                        File.Delete(binfile);
+                    }
+                    if (b == null)
+                    {
+                        VSUtil.LogError("Solidity Compiler", "Error reading Solidity compiler output: could not find compiler output file for " + file + ".");
+
+                    }
+
                     return;
                 }
+                
 
             }
         }
