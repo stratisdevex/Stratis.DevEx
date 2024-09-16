bd12b10c21f2066f2f51a4b8e8ff6e917acbd3ce
Allister Beharry <allister.beharry@gmail.com>
2024-09-16 17:18:35 -0400

    Add XAML controls to wizard.

--- src/Stratis.VS.StratisEVM/Window1.xaml.cs
+++ src/Stratis.VS.StratisEVM/Window1.xaml.cs
@@ -25,5 +25,14 @@ namespace Stratis.VS.StratisEVM
             InitializeComponent();
             //InitializeComponent();
         }
+
+        public string SelectedConfigFile { get; set; }
+
+
+        private void okButton_Click(object sender, RoutedEventArgs e) =>
+    DialogResult = true;
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e) =>
+            DialogResult = false;
     }
 }
