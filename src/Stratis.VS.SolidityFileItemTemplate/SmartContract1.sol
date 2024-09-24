7b41b0ed6e3b7ef7c4d00b2f638da49935045c75
Allister Beharry <allister.beharry@gmail.com>
2024-09-24 10:19:56 -0400

    Add Solidity FileItemTemplate.

---
+++ src/Stratis.VS.SolidityFileItemTemplate/SmartContract1.sol
@@ -0,0 +1,14 @@
+﻿// SPDX-License-Identifier: GPL-3.0
+pragma solidity 0.8.20;
+
+contract SimpleStorage {
+    uint storedData;
+
+    function set(uint x) public {
+        storedData = x;
+    }
+
+    function get() public view returns (uint) {
+        return storedData;
+    }
+}
