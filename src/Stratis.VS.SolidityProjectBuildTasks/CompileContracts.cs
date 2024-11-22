b527d8e86cc4d2a462f255fc31dec7920c3f9f88
Allister Beharry <allister.beharry@gmail.com>
2024-11-22 17:48:40 -0400

    Begin CompileContracts with solcjs.

--- src/Stratis.VS.SolidityProjectBuildTasks/CompileContracts.cs
+++ src/Stratis.VS.SolidityProjectBuildTasks/CompileContracts.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using Microsoft.Build.Framework;
 using Microsoft.Build.Utilities;
@@ -18,8 +20,24 @@ namespace Stratis.VS
 
         public override bool Execute()
         {
-            Log.LogMessage(MessageImportance.High, "{0}:{1}", ExtDir, Contracts.Select(c => c.ItemSpec).ToArray());
-            return true;
+            var solcpath = Directory.Exists(Path.Combine(ProjectDir, "node_modules", "solc", "solc.js")) ? Path.Combine(ProjectDir, "node_modules", "solc", "solc.js") :
+                Path.Combine(ExtDir, "node_modules", "solc", "solc.js");
+            Log.LogMessage(MessageImportance.High, "Compiling {0} files in directory:{1}", Contracts.Count(), ProjectDir);
+           
+
+            var psi = new ProcessStartInfo("cmd.exe", "/c node" + solcpath + " --base-path=\"" + ProjectDir + "\"")
+            {
+                WorkingDirectory = ProjectDir,
+                CreateNoWindow = true,  
+                RedirectStandardOutput = false,  
+                RedirectStandardInput = false,   
+                RedirectStandardError = false,
+            };
+            var p = new Process()
+            {
+                StartInfo = psi   
+            };
+            return false;
         }
     }
 }
