426f08407438b820627411126125aa5cc4a35f6c
Allister Beharry <allister.beharry@gmail.com>
2025-02-23 17:21:41 -0400

    Move MainWndow to BlockchainExplorer folder. Add IsNetworkError to Result.

---
+++ src/Stratis.DevEx.Ethereum/Extensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Stratis.DevEx.Ethereum
+{
+    public static class Extensions
+    {
+        public static bool IsNetworkError<T>(this Result<T> result)
+        {
+            if (result.Exception is null)
+            {
+                return false;
+            }
+            else
+            {
+                var n = result.Exception.GetType().Name;
+                return n.Contains("RpcClient") || n.Contains(("Http"));  
+            }
+        }
+    }
+}
