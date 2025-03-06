c448ff45eb85c5b910ae479eb4e0b22fe4c42da9
Allister Beharry <allister.beharry@gmail.com>
2025-03-06 13:32:49 -0400

    Update blockchain icon.

--- tests/Stratis.DevEx.Wpf.Test/BlockchainExplorer/BlockchainExplorerTree.cs
+++ tests/Stratis.DevEx.Wpf.Test/BlockchainExplorer/BlockchainExplorerTree.cs
@@ -1,32 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-using System.Windows;
 using System.Windows.Input;
-using System.Windows.Data;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
 
 using Hardcodet.Wpf.GenericTreeView;
 
-using Stratis.VS.StratisEVM.UI.ViewModel;
-using System.Windows.Controls;
 using Stratis.DevEx;
-using System.Windows.Media.Imaging;
+using Stratis.VS.StratisEVM.UI.ViewModel;
 
 namespace Stratis.VS.StratisEVM.UI
 {
     public class BlockchainExplorerTree : TreeViewBase<BlockchainInfo>
     {
-        public override string GetItemKey(BlockchainInfo item) => (item.Parent?.Name) ?? "Root" + "_" + item.Kind + "_" + item.Name;
-
-        public override ICollection<BlockchainInfo> GetChildItems(BlockchainInfo parent) => parent.Children;
-
-        public override BlockchainInfo GetParentItem(BlockchainInfo item) => item.Parent;
-
-        public static RoutedCommand NewNetworkCmd = new RoutedCommand();
+        #region Properties
+        public static RoutedCommand NewNetworkCmd { get; } = new RoutedCommand();
 
         public static BitmapImage FolderClosedIcon { get; } = new BitmapImage(new Uri(Runtime.AssemblyLocation.CombinePath("Images", "FolderClosed.png")));
 
@@ -43,17 +31,25 @@ namespace Stratis.VS.StratisEVM.UI
         public static BitmapImage GlobeIcon { get; } = new BitmapImage(new Uri(Runtime.AssemblyLocation.CombinePath("Images", "Globe.png")));
 
         public static BitmapImage UrlIcon { get; } = new BitmapImage(new Uri(Runtime.AssemblyLocation.CombinePath("Images", "Url.png")));
+        #endregion
+
+        #region Methods
+        public override string GetItemKey(BlockchainInfo item) => (item.Parent?.Name) ?? "Root" + "_" + item.Kind + "_" + item.Name;
+
+        public override ICollection<BlockchainInfo> GetChildItems(BlockchainInfo parent) => parent.Children;
+
+        public override BlockchainInfo GetParentItem(BlockchainInfo item) => item.Parent;
 
         protected override TreeViewItem CreateTreeViewItem(BlockchainInfo data)
         {
-            var item =  base.CreateTreeViewItem(data);
+            var item = base.CreateTreeViewItem(data);
             if (data.Kind == BlockchainInfoKind.Network)
             {
-                item.ContextMenu = (ContextMenu) TryFindResource("TreeMenu2");
+                item.ContextMenu = (ContextMenu)TryFindResource("TreeMenu2");
             }
             return item;
         }
-    }
+        #endregion
 
-    
+    }    
 }
