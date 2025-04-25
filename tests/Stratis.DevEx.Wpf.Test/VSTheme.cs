801dd3a69f1237abd11b686c9820f7926b2d6c7c
Allister Beharry <allister.beharry@gmail.com>
2025-04-25 05:30:14 -0400

    Migrate BET changes to WPF test project.

---
+++ tests/Stratis.DevEx.Wpf.Test/VSTheme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Wpf.Ui;
+
+namespace Stratis.VS.StratisEVM.UI
+{
+    public class VSTheme
+    {
+        public static object ToolWindowTextKey => Wpf.Ui.Markup.ThemeResource.TextFillColorPrimaryBrush.ToString();
+        public static object ToolWindowBackgroundKey => Wpf.Ui.Markup.ThemeResource.ApplicationBackgroundBrush;
+
+        public static void WatchThemeChanges()
+        {
+            
+        }
+    }
+}
